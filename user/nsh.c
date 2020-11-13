21c1b40a8c86994abf6988882447d5ee2ff2d09d
Ldq369 <ldq2000725@126.com>
2020-11-13 12:19:39 +0800

    Lab2 1.1 push,unfinished.

--- user/nsh.c
+++ user/nsh.c
@@ -175,7 +175,7 @@ int fork1(void)
 }
 
 //without malloc!!!
-/*
+
 struct cmd* ececcmd(void)
 {
     struct execcmd *cmd;
@@ -183,7 +183,36 @@ struct cmd* ececcmd(void)
     cmd->type = EXEC;
     return (struct cmd*)cmd;
 }
-*/
+/*
+struct cmd*
+redircmd(struct cmd *subcmd, char *file, char *efile, int mode, int fd)
+{
+  struct redircmd *cmd;
+
+  cmd = malloc(sizeof(*cmd));
+  memset(cmd, 0, sizeof(*cmd));
+  cmd->type = REDIR;
+  cmd->cmd = subcmd;
+  cmd->file = file;
+  cmd->efile = efile;
+  cmd->mode = mode;
+  cmd->fd = fd;
+  return (struct cmd*)cmd;
+}
+
+struct cmd*
+pipecmd(struct cmd *left, struct cmd *right)
+{
+  struct pipecmd *cmd;
+
+  cmd = malloc(sizeof(*cmd));
+  memset(cmd, 0, sizeof(*cmd));
+  cmd->type = PIPE;
+  cmd->left = left;
+  cmd->right = right;
+  return (struct cmd*)cmd;
+}*/
+
 
 //Parsing
 
